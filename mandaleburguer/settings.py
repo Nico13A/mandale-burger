dc337638cffe9fb94613fd73981ac6d02d3f571d
Nico13A <nicoantinao1998@gmail.com>
2025-09-24 12:07:09 -0300

    Modificando la conexión con la base de datos

--- mandaleburguer/settings.py
+++ mandaleburguer/settings.py
@@ -83,10 +83,22 @@ WSGI_APPLICATION = 'mandaleburguer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mandale_burger',        
+        'USER': 'postgres',             
+        'PASSWORD': 'fence8lear',    
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
