69c05b1e72f701c70a10db79ad269420d693ea95
Nico13A <nicoantinao1998@gmail.com>
2025-10-01 01:24:20 -0300

    Agrega buscador

--- mandaleburguer-frontend/src/pages/Admin/CocinerosList.jsx
+++ mandaleburguer-frontend/src/pages/Admin/CocinerosList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useCocineros } from "../../hooks/useCocineros";
 import { useCocineroDelDia } from "../../hooks/useCocineroDelDia";
 import { getCocineroDelDiaActual } from "../../services/cocineroDia";
@@ -20,6 +20,7 @@ const CocinerosList = () => {
   const { asignarCocineroDelDia } = useCocineroDelDia();
   const [cocineroDelDiaId, setCocineroDelDiaId] = useState(null);
   const [loadingId, setLoadingId] = useState({ id: null, action: null });
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -64,6 +65,29 @@ const CocinerosList = () => {
     }
   };
 
+  // ---------------- Filtrado ----------------
+  const cocinerosActivosFiltrados = useMemo(() => {
+    if (!searchTerm) return cocinerosActivos;
+    const palabras = searchTerm.toLowerCase().split(" ").filter(Boolean);
+    return cocinerosActivos.filter((c) =>
+      palabras.every(
+        (palabra) =>
+          c.first_name.toLowerCase().includes(palabra) ||
+          c.last_name.toLowerCase().includes(palabra)
+      )
+    );
+  }, [searchTerm, cocinerosActivos]);
+
+  const cocinerosInactivosFiltrados = useMemo(() => {
+    if (!searchTerm) return cocinerosInactivos;
+    const palabras = searchTerm.toLowerCase().split(" ").filter(Boolean);
+    return cocinerosInactivos.filter((c) =>
+      palabras.every(palabra => c.first_name.toLowerCase().includes(palabra) || c.last_name.toLowerCase().includes(palabra))
+    );
+  }, [searchTerm, cocinerosInactivos]);
+
+
+  // ---------------- Render filas ----------------
   const renderCocineroRow = (c, isActive = true) => (
     <tr
       key={c.id}
@@ -98,9 +122,8 @@ const CocinerosList = () => {
   const renderCocineroCard = (c, isActive = true) => (
     <div
       key={c.id}
-      className={`bg-white p-4 rounded-2xl shadow-sm hover:shadow-lg transition-shadow duration-200 space-y-2 ${
-        cocineroDelDiaId === c.id ? "bg-yellow-100" : ""
-      }`}
+      className={`bg-white p-4 rounded-2xl shadow-sm hover:shadow-lg transition-shadow duration-200 space-y-2 ${cocineroDelDiaId === c.id ? "bg-yellow-100" : ""
+        }`}
     >
       {c.profile?.image ? (
         <img
@@ -139,25 +162,43 @@ const CocinerosList = () => {
 
   return (
     <div className="w-full max-w-4xl mx-auto mt-6 pb-25 md:pb-0">
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center space-x-2 mb-4 md:mb-6">
         <h1 className="text-2xl md:text-3xl font-bold">Cocineros</h1>
-        <button
-          onClick={() => navigate("/admin/cocineros/nuevo")}
-          className="flex items-center px-4 py-2 text-white rounded-2xl bg-naranja-boton hover:bg-naranja-boton-hover cursor-pointer"
-        >
-          <PlusIcon className="w-5 h-5 mr-2" />
-          Agregar
-        </button>
+        <div className="flex space-x-2 items-center">
+          <input
+            type="text"
+            placeholder="Buscar por nombre o apellido"
+            className="hidden md:block px-4 py-2 w-64 md:w-72 border border-gray-300 rounded-2xl focus:outline-none focus:ring-1 focus:ring-botoborder-gris-boton focus:border-gris-boton transition-all duration-200"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button
+            onClick={() => navigate("/admin/cocineros/nuevo")}
+            className="flex items-center px-4 py-2 text-white rounded-2xl bg-naranja-boton hover:bg-naranja-boton-hover cursor-pointer"
+          >
+            <PlusIcon className="w-5 h-5 mr-2" />
+            Agregar
+          </button>
+        </div>
       </div>
+
+      {/* Para no romper en mobile */}
+      <input
+        type="text"
+        placeholder="Buscar por nombre o apellido"
+        className="md:hidden px-4 py-2 w-full border border-gray-300 rounded-2xl focus:outline-none focus:ring-1 focus:ring-botoborder-gris-boton focus:border-gris-boton transition-all duration-200"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
 
       {loadingList && <Loading />}
       {error && <p className="text-red-500">{error}</p>}
 
-      {!loadingList && cocinerosActivos.length > 0 && (
+      {!loadingList && cocinerosActivosFiltrados.length > 0 && (
         <section className="w-full">
           <h2 className="font-medium text-lg md:text-2xl mt-6 mb-2 text-orange-400 flex items-center space-x-2">
-              <CheckCircleIcon className="w-5 h-5 text-orange-400" />
-              <span>Lista de activos</span>
+            <CheckCircleIcon className="w-5 h-5 text-orange-400" />
+            <span>Lista de activos</span>
           </h2>
           <div className="hidden overflow-x-auto md:block md:rounded-lg md:shadow-md">
             <table className="min-w-full bg-white table-auto">
@@ -171,18 +212,18 @@ const CocinerosList = () => {
                 </tr>
               </thead>
               <tbody>
-                {cocinerosActivos.map((c) => renderCocineroRow(c))}
+                {cocinerosActivosFiltrados.map((c) => renderCocineroRow(c))}
               </tbody>
             </table>
           </div>
 
           <div className="md:hidden flex flex-col space-y-3">
-            {cocinerosActivos.map((c) => renderCocineroCard(c))}
+            {cocinerosActivosFiltrados.map((c) => renderCocineroCard(c))}
           </div>
         </section>
       )}
 
-      {!loadingList && cocinerosInactivos.length > 0 && (
+      {!loadingList && cocinerosInactivosFiltrados.length > 0 && (
         <section className="w-full">
           <h2 className="font-medium text-lg md:text-2xl mt-6 mb-2 text-red-400 flex items-center space-x-2">
             <XCircleIcon className="w-5 h-5 text-red-400" />
@@ -200,20 +241,22 @@ const CocinerosList = () => {
                 </tr>
               </thead>
               <tbody>
-                {cocinerosInactivos.map((c) => renderCocineroRow(c, false))}
+                {cocinerosInactivosFiltrados.map((c) => renderCocineroRow(c, false))}
               </tbody>
             </table>
           </div>
 
           <div className="md:hidden flex flex-col space-y-3">
-            {cocinerosInactivos.map((c) => renderCocineroCard(c, false))}
+            {cocinerosInactivosFiltrados.map((c) => renderCocineroCard(c, false))}
           </div>
         </section>
       )}
 
-      {!loadingList && cocinerosActivos.length === 0 && cocinerosInactivos.length === 0 && (
-        <p>No hay cocineros registrados.</p>
-      )}
+      {!loadingList &&
+        cocinerosActivosFiltrados.length === 0 &&
+        cocinerosInactivosFiltrados.length === 0 && (
+          <p>No hay cocineros registrados.</p>
+        )}
     </div>
   );
 };
